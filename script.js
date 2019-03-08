f04c36a4241d9a3948da25ee88545fddaeb1a251
Jacob Whisenant <jacobwhise@gmail.com>
2019-03-08 00:23:24 -0700

    First Commit

--- script.js
+++ script.js
@@ -1,113 +1,50 @@
-var now = moment().format('MMMM Do YYYY, h:mm:ss a');
-document.getElementById("demo").innerHTML = "Submitted: " + now;
-Vue.component('star-rating', VueStarRating.default);
-
-let app = new Vue({
+new Vue({
   el: '#app',
-  data: {
-    number: '',
-    max: '',
-    current: {
-      title: '',
-      img: '',
-      alt: ''
-    },
-    loading: true,
-    addedName: '',
-    addedComment: '',
-    comments: {},
-    ratings: {},
-    currentRating: '',
-  },
-  created() {
-    this.xkcd();
-  },
-  methods: {
-    async xkcd() {
-      try {
-        this.loading = true;
-        const response = await axios.get('https://xkcd.now.sh/' + this.number);
-        this.current = response.data;
-        this.loading = false;
-        this.number = response.data.num;
-
-      } catch (error) {
-        console.log(error);
-      }
-    },
-
-    firstComic(min) {
-      this.number = 1;
-    },
-    lastComic() {
-      this.number = this.max;
-    },
-    previousComic() {
-      this.number = this.current.num - 1;
-      if (this.number < 1)
-        this.number = 1;
-    },
-    nextComic() {
-      this.number = this.current.num + 1;
-      if (this.number > this.max)
-        this.number = this.max
-    },
-    getRandom(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum and minimum are inclusive
-    },
-    randomComic() {
-      this.number = this.getRandom(1, this.max);
-    },
-    addComment() {
-      if (!(this.number in this.comments))
-        Vue.set(app.comments, this.number, new Array);
-      this.comments[this.number].push({
-        author: this.addedName,
-        text: this.addedComment,
-      });
-      this.addedName = '';
-      this.addedComment = '';
-    },
-    setRating(rating) {
-      if (!(this.number in this.ratings))
-        Vue.set(this.ratings, this.number, {
-          sum: 0,
-          total: 0,
-        });
-      this.ratings[this.number].sum += rating;
-      this.ratings[this.number].total += 1;
-      Vue.set(this.currentRating ,this.ratings[this.number].sum / this.ratings[this.number].total);
-    },
-  },
-  watch: {
-    number(value, oldvalue) {
-      if (oldvalue === '') {
-        this.max = value;
-      } else {
-        this.xkcd();
-      }
-    },
-  },
-  computed: {
-    month() {
-      var month = new Array;
-      if (this.current.month === undefined)
-        return '';
-      month[0] = "January";
-      month[1] = "February";
-      month[2] = "March";
-      month[3] = "April";
-      month[4] = "May";
-      month[5] = "June";
-      month[6] = "July";
-      month[7] = "August";
-      month[8] = "September";
-      month[9] = "October";
-      month[10] = "November";
-      month[11] = "December";
-      return month[this.current.month - 1];
+  data() {
+    return {
+      info: null
     }
   },
-});
+  methods: {
+    d100() {
+      axios
+        .get('http://roll.diceapi.com/json/d100')
+        .then(response => (this.info = response.data.dice))
+    },
+    d20() {
+      axios
+        .get('http://roll.diceapi.com/json/d20')
+        .then(response => (this.info = response.data.dice))
+    },
+    d12() {
+      axios
+        .get('http://roll.diceapi.com/json/d12')
+        .then(response => (this.info = response.data.dice))
+    },
+    d10() {
+      axios
+        .get('http://roll.diceapi.com/json/d10')
+        .then(response => (this.info = response.data.dice))
+    },
+    d8() {
+      axios
+        .get('http://roll.diceapi.com/json/d8')
+        .then(response => (this.info = response.data.dice))
+    },
+    d6() {
+      axios
+        .get('http://roll.diceapi.com/json/d6')
+        .then(response => (this.info = response.data.dice))
+    },
+    d4() {
+      axios
+        .get('http://roll.diceapi.com/json/d4')
+        .then(response => (this.info = response.data.dice))
+    },
+    d2() {
+      axios
+        .get('http://roll.diceapi.com/json/d2')
+        .then(response => (this.info = response.data.dice))
+    },
+  }
+})
